9c48b80eda7f28fb794e6ff0da88bbb2f8fc952c
sofiapedros <202108146@alu.comillas.edu>
2022-11-08 08:33:21 +0100

    merged

--- Blockchain.py
+++ Blockchain.py
@@ -1,6 +1,20 @@
 import json
 import hashlib
 import time
+
+class Bloque:
+    def __init__(self, indice: int, transacciones: list[dict], timestamp: float, hash_previo: str, prueba: int =0):
+        self.indice = indice
+        self.transacciones = transacciones
+        self.timestamp = timestamp
+        self.hash_previo = hash_previo
+        self.prueba = prueba
+        self.hash = self.calcular_hash()
+    # Codigo a completar (inicializacion de los elementos del bloque)
+    def calcular_hash(self) -> str:
+        block_string =json.dumps(self.__dict__, sort_keys=True)
+        return hashlib.sha256(block_string.encode()).hexdigest()
+
 class Blockchain(object):
 
     def __init__(self):
@@ -36,15 +50,3 @@ class Blockchain(object):
 
     def integra_bloque(self, bloque_nuevo: Bloque, hash_prueba: str) ->bool:
         pass
-class Bloque:
-    def __init__(self, indice: int, transacciones: list[dict], timestamp: float, hash_previo: str, prueba: int =0):
-        self.indice = indice
-        self.transacciones = transacciones
-        self.timestamp = timestamp
-        self.hash_previo = hash_previo
-        self.prueba = prueba
-        self.hash = self.calcular_hash()
-    # Codigo a completar (inicializacion de los elementos del bloque)
-    def calcular_hash(self) -> str:
-        block_string =json.dumps(self.__dict__, sort_keys=True)
-        return hashlib.sha256(block_string.encode()).hexdigest()
